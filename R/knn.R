b52b3ad6eaadba2cb4e409f666c4eec9582e392a
chloebates4 <chloe.bates4@gmail.com>
2019-02-15 04:02:51 -0700

    Deleted one of the two knn.R

--- R/knn.R
+++ R/knn.R
@@ -1,9 +1,13 @@
-#' K nearest neighbors algo
+#' K nearest neighbors algorithm
 #'
-#'R function that wraps our C++ code. 
+#' A demo R function that wraps our C++ code. 
+#' 
+#' NEED TO WRITE OUR OWN EXAMPLES!!! CHANGE! 
 #'
 #' @param X.mat numeric train feature matrix [n x p]
-#' @param y.vec numeric train label vector [n], either all 0/1 for binary classification, or other real numbers for regression (multi-class classification not supposrted).
+#' @param y.vec numeric train label vector [n], either 
+#' all 0/1 for binary classification, or other real numbers
+#' for regression (multi-class classification not supposrted).
 #' @param testX.vec numeric test feature vector [p]
 #' @param max.neighbors scalar integer, max number of neighbors.
 #'
@@ -12,10 +16,10 @@
 #'
 #' @examples 
 #' data(zip.train, package="ElemStatLearn")
-#' i01 <- chich(zip.train[,1] %in% c(0,1))
+#' i01 <- which(zip.train[,1] %in% c(0,1))
 #' train.i <- i01[1:5]
 #' test.i <- i01[6]
-#' x <- zip.train[train.i, -1]
+#' X <- zip.train[train.i, -1]
 #' y <- zip.train[train.i, 1]
 #' testX <- zip.train[test.i, -1]
 #' knn(X, y, testX, 3)
@@ -25,6 +29,6 @@ knn <- function(X.mat, y.vec, testX.vec, max.neighbors){
     "knn_interface", as.double(x.mat), as.double(y.vec), 
     as.double(testX.vec), as.integer(nrow(X.mat)), 
     as.integer(ncol(X.mat)), as.integer(max.neighbors), 
-    predictions-double(max.neighbors), PACKAGE="NearestNeighbors")
+    predictions-double(max.neighbors), PACKAGE="RstuffDelete")
   result.list$predictions
 }
